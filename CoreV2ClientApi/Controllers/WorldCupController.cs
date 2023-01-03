bc2172fd777118c7ea484c0381f311e36ef76fbc
= <=>
2023-01-03 11:06:02 +0100

    Add project files.

---
+++ CoreV2ClientApi/Controllers/WorldCupController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreV2ClientApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WorldCupController : ControllerBase
+{
+    [HttpGet("GetWinner")]
+    public async Task<ActionResult<Task<string>>> GetWinnerAsync()
+    {
+        return await Task.FromResult(Ok("Argentina"));
+    }
+}
---
+++ CoreV2ClientApi/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+var appName = typeof(Program).Assembly.GetName().Name;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(x =>
+{
+    x.Title = appName;
+    x.Version = "1.0.0";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
